8ce01c36464b5acb08309522843b838c8598da3f
shivprasadnikam <shivvprasad.nikam@gmail.com>
2023-12-11 20:12:07 +0530

    inital commit

---
+++ 1-Counter.js
@@ -0,0 +1,18 @@
+// Create a counter in JavaScript
+// We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript It should go up as time goes by in intervals of 1 second
+
+/*
+Ans-> So we need to create a function which starts after every one sec by using time
+
+*/
+console.log("Program Started !!!");
+function counter(){
+    console.log("Program Starting After Every One Second !!!");
+
+    for(var i=0; i<10; i++)
+     {
+    console.log(i);
+    }
+
+}
+setInterval(counter,1000);
